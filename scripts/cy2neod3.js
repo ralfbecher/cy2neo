a1d3685eccb163a5e503fe839a124eed8c894e1f
Ralf Becher <ralf.becher@tiq-solutions.de>
2014-11-29 21:02:34 +0100

    sweetAlert error messages, execute button spinning

--- scripts/cy2neod3.js
+++ scripts/cy2neod3.js
@@ -18,17 +18,30 @@ function Cy2NeoD3(config, graphId, sourceId, execId, urlSource) {
 			evt.preventDefault();
 			var query = editor.getValue();
 			console.log("Executing Query",query);
+			var execButton = $(this).find('i');
+			execButton.toggleClass('fa-play-circle-o fa-spinner fa-spin')
 			neo.executeQuery(query,{},function(err,res) {
+				execButton.toggleClass('fa-spinner fa-spin fa-play-circle-o')
 				res = res || {}
 				var graph=res.graph;
 				if (graph) {
 					var c=$("#"+graphId);
 					c.empty();
 					neod3.render(graphId, c ,graph);
+				} else {
+					if (err) {
+						console.log(err);
+						if (err.length > 0) {
+							sweetAlert("Cypher error", err[0].code + "\n" + err[0].message, "error");
+						} else {
+							sweetAlert("Ajax " + err.statusText, "Status " + err.status + ": " + err.state(), "error");
+						}
+					}
 				}
 			});
 		} catch(e) {
 			console.log(e);
+			sweetAlert("Catched error", e, "error");
 		}
 		return false;
 	});
